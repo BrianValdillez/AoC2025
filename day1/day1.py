d47e7933577b308caf433e9c7d2c8d5e2fa6ea6b
Brian Valdillez <valdillez@alumni.usc.edu>
2025-12-05 00:32:14 -0500

    Day 1: Second half solved.

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,5 @@
+import sys
+
 FILEPATH = "input.txt"
 STARTING_VALUE = 50
 TARGET_VALUE = 0
@@ -8,24 +10,47 @@ def read_file(filepath):
     return ""
 
 def main():
+    # Check for alternate mode
+    alt = False
+    if len(sys.argv) >= 2 and sys.argv[1] == 'B':
+        alt = True
+
     # Load safe data and stick into a list.
     input = read_file(FILEPATH)
     safe_inputs = input.split("\n")
-    print(safe_inputs)
+    #print(safe_inputs)
 
     dial_value = STARTING_VALUE
     print(f"- The dial starts by pointing at {STARTING_VALUE}.")
 
     target_count = 0
+    passby_count = 0
     for input in safe_inputs:
         input_direction = 1 if input[0] == 'R' else -1
-        input_value = int("".join(input[1::]))
-        dial_value = (dial_value + (input_direction * input_value) + 100) % 100
+        input_value = int(input[1:])
 
-        print(f"- The dial is rotated {input} to point at {dial_value}.")
-        if dial_value == TARGET_VALUE:
-            target_count += 1
+        passby_curr = 0
+
+        # Check for the number of full rotations
+        while (input_value > 100):
+            input_value -= 100
+            passby_curr += 1
+
+        if (input_value > 0):
+            dial_prev = dial_value
+            dial_value = (dial_value + (input_direction * input_value) + 100) % 100
+
+            if dial_value == TARGET_VALUE:
+                target_count += 1
+            elif input_direction > 0 and dial_prev != 0 and dial_value < dial_prev:
+                passby_curr += 1
+            elif input_direction < 0 and dial_prev != 0 and dial_value > dial_prev:
+                passby_curr += 1
+
+        passby_count += passby_curr
+        print(f"- The dial is rotated {input} to point at {dial_value}, and passed the target {passby_curr} times. COUNT: {passby_count + target_count}")
         
-    print(f"We hit our target value {TARGET_VALUE} a total of {target_count} times!")
+    print(f"Mode A: We hit our target value {TARGET_VALUE} a total of {target_count} times!")
+    print(f"Mode B: We hit our target value {TARGET_VALUE} a total of {target_count + passby_count} times!")
 
 main()
